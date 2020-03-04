f35378459d1f3bef264913d105ac26a9d5bc5f89
Echo127 <echo127@gmail.com>
2020-03-05 12:32:56 +1300

    add state

--- src/app.ts
+++ src/app.ts
@@ -1,62 +1,8 @@
 /// <reference path="models/drag-drop.ts"/>
 /// <reference path="models/project.ts"/>
+/// <reference path="state/project-state.ts"/>
 
 namespace App {
-  type Listener<T> = (items: T[]) => void;
-  class State<T> {
-    protected listeners: Listener<T>[] = [];
-    addListener(listnerFn: Listener<T>) {
-      this.listeners.push(listnerFn);
-    }
-  }
-
-  //Project State Management
-  class ProjectState extends State<Project> {
-    private projects: Project[] = [];
-    private static instance: ProjectState;
-
-    private constructor() {
-      super();
-    }
-
-    static getInstance() {
-      if (this.instance) {
-        return this.instance;
-      } else {
-        this.instance = new ProjectState();
-      }
-      return this.instance;
-    }
-
-    addProject(title: string, description: string, numOfPeople: number) {
-      const newProject = new Project(
-        Math.random().toString(),
-        title,
-        description,
-        numOfPeople,
-        projectStatus.Active
-      );
-      this.projects.push(newProject);
-      this.undateListeners();
-    }
-
-    moveProject(prjId: string, newStatus: projectStatus) {
-      const project = this.projects.find(prj => prj.id === prjId);
-      if (project && project.status !== newStatus) {
-        project.status = newStatus; //change the oject in array
-        this.undateListeners();
-      }
-    }
-    private undateListeners() {
-      for (const listerFn of this.listeners) {
-        //slice() is a copy of projects, so we will not change projects
-        listerFn(this.projects.slice());
-      }
-    }
-  }
-
-  const projectState = ProjectState.getInstance();
-
   //validation
   interface Validatable {
     value: string | number;
